14e439484ca2dce50c6f337b3b49aa0c8ff18d50
Naman Batra <naman@Namans-MacBook-Air.local>
2023-09-02 15:22:54 +0530

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
       <section className="bg-richblack-700">
         <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-center text-white">
           <header className="mx-auto py-20 text-4xl font-semibold lg:w-[70%]">
-            Driving Innovation in Online Education for a
+            A personal project made by 
             <HighlightText text={"Brighter Future"} />
             <p className="mx-auto mt-3 text-center text-base font-medium text-richblack-300 lg:w-[95%]">
               Studynotion is at the forefront of driving innovation in online
